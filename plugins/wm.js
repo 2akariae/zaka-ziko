92abaf6ce75bd4173c7c173cdf2e35bcdb8305cb
2akariae <zakariaelahrache2@gmail.com>
2026-02-01 23:56:06 +0100

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -1,16 +1,16 @@
 import { addExif } from "../lib/sticker.js";
 
 let handler = async (m, { conn, text }) => {
-  if (!m.quoted) throw "قم بالاشارة للملصق التي تريد تغيير حقوقه يا عزيزي!";
+  if (!m.quoted) throw "tagi sticker";
   let stiker = false;
   try {
     let [packname, ...author] = text.split("|");
     author = (author || []).join("|");
     let mime = m.quoted.mimetype || "";
     if (!/webp/.test(mime))
-      throw "قم بالاشارة للصورة التي تريد تغيير حقوقها يا عزيزي !";
+      throw "tagi chi pic!";
     let img = await m.quoted.download();
-    if (!img) throw "قم بالاشارة للملصق التي تريد تغيير حقوقه يا عزيزي!";
+    if (!img) throw "tagi stickerrrrrr";
     stiker = await addExif(img, packname || "", author || "");
   } catch (e) {
     console.error(e);
